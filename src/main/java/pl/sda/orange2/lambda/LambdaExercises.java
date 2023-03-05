2663b6d24c56a6c126804e4ca66914bfecf56aa2
NiekompatybilnaM <milenasmolska@gmail.com>
2023-03-05 07:30:33 +0100

    [change] - lambda with input parameters and returned value

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -1,7 +1,5 @@
 package pl.sda.orange2.lambda;
 
-import javax.sound.midi.Soundbank;
-
 public class LambdaExercises {
 
     public static void main(String[] args) {
@@ -12,45 +10,44 @@ public class LambdaExercises {
         ChłopakiNiePłaczą boy2 = new ChłopakiNiePłaczą();
         boy2.sing();
 
-        //anonymous class
+        // anonymous class
         BoysDontCry anonymousClassReference = new BoysDontCry() {
             @Override
             public void silnoreki() {
                 System.out.println("Anonimowy silnoreki:)");
             }
         };
+
         anonymousClassReference.silnoreki();
 
+//        BoysDontCry stupid = new String("");
 
-        // () parenthesis = arguments list
+        // () parenthesis - arguments list
         // -> arguments, body separator
         // {} parenthesis - method body
-        //metoda nic nie przyjmuje i nic nie zwraca
-
-        BoysDontCry firstLambda = () -> {
-        };
+        BoysDontCry firstLambda = () -> {};
         BoysDontCry secondLambda = () -> System.out.println("Lambda:)");
         secondLambda.silnoreki();
-        BoysDontCry thirdLambda = () ->
-        {
+        BoysDontCry thirdLambda = () -> {
             System.out.println("one");
             System.out.println("second");
-
-            System.out.println("------------------");
-            System.out.println("Coffee time");
-
-            CoffeeMaker myCoffeeMaker = (int water, String coffeeType) -> "my coffee";
-            CoffeeMaker myCoffeeMaker2 = (int water, String coffeeType) -> {
-                return "my coffee";
-            };
-
-            CoffeeMaker myCoffeeMaker3 = (int water, String coffeeType) -> {
-                System.out.println("Coffee brewing...");
-                return "my coffee";
-            };
-
-            CoffeeMaker shortVersion = (water, coffeeType) -> "my coffee";
-            shortVersion.prepare(5,"Large Java coffee");
-
         };
-    }}
+
+        System.out.println("----------------------------");
+        System.out.println("Coffee time");
+
+        CoffeeMaker myCoffeeMaker = (int water, String coffeeType) -> "my coffee";
+        CoffeeMaker myCoffeeMaker2 = (int water, String coffeeType) -> {
+            return "my coffee";
+        };
+        CoffeeMaker myCoffeeMaker3 = (int water, String coffeeType) -> {
+            System.out.println("Coffee brewing...");
+            return "Java Latte";
+        };
+        String myCoffee = myCoffeeMaker3.prepare(5, "Small");
+        System.out.println("my coffee is ready: " + myCoffee);
+
+        CoffeeMaker shortVersion = (water, coffeeType) -> "my coffee";
+        shortVersion.prepare(5, "Large Java coffee");
+    }
+}
